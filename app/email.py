f25e51396dcb7f223ed15e699ff7ca254db31fdf
obadiaH123 <benard.bett@student.moringaschool.com>
2021-09-28 10:23:26 +0300

    adding emails.py

--- app/email.py
+++ app/email.py
@@ -3,11 +3,11 @@ from flask import render_template
 from . import mail
 
 subject_pref = 'Awesome Blogs'
-sender_email  ='kelvin.cheruiyot@student.moringaschool.com'
+sender_email  ='benard.bett@student.moringaschool.com'
 
 
 def mail_message(subject,template,to,**kwargs):    
-    sender_email  ='kelvin.cheruiyot@student.moringaschool.com'
+    sender_email  ='benard.bett@student.moringaschool.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
